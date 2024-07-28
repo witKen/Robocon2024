e0edd71cb69f9fa749363d9846e361706f5cdcbd
witKen <Sokkenkh12@gmail.com>
2024-07-28 15:55:10 +0700

    Added Fileration

--- Color Detection/colorDetection.py
+++ Color Detection/colorDetection.py
@@ -1,297 +1,10 @@
-# import cv2
-# import numpy as np
-
-# def findLargestContour(contours):
-#     maxArea = 0
-#     minArea = 400
-#     largestContour = None
-#     for contour in contours:
-#         area = cv2.contourArea(contour)
-#         if area > maxArea and area > minArea: 
-#             # moments = cv2.moments(contour)
-#             # cX = int(moments["m10"] / moments["m00"])
-#             # cY = int(moments["m01"] / moments["m00"])
-
-#             # if iLastX >= 0 and iLastY >= 0 and cX >= 0 and cY >= 0:
-#             #     cv2.line(frame, (cX, cY), (iLastX, iLastY), (0, 0, 255), 2)
-
-#             # iLastX = cX
-#             # iLastY = cY
-
-#             maxArea = area
-#             largestContour = contour
-#     return largestContour
-# #     if largestContour is not None:
-# #        approx = cv2.approxPolyDP(largestContour, 0.01 * cv2.arcLength(largestContour, True), True)
-# #        if len(approx) >= 9:
-# #            print("is Ball")
-# #            return largestContour
-# #    return None
-
-# iLastX = -1
-# iLastY = -1
-
-# lower_red1 = np.array([0,70,50])
-# upper_red1 = np.array([10,255,255])
-
-# #blue range
-# lower_blue = np.array([110,50,50])
-# upper_blue = np.array([130,255,255])
-
-# # #Red range
-# # lower_red1 = np.array([0,31,255])
-# # upper_red1 = np.array([176,255,255])
-
-# ballWidth = 190
-
-# cap = cv2.VideoCapture(0)
-
-# while True:
-#     re, frame = cap.read()
-
-#     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-#     mask = cv2.inRange(hsv_frame, lower_red1, upper_red1)
-
-#     # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-#     # mask = cv2.erode(mask, kernel, iterations=1)
-#     # mask = cv2.dilate(mask, kernel, iterations=1)
-#     # mask = cv2.dilate(mask, kernel, iterations=1)
-#     # mask = cv2.erode(mask, kernel, iterations=1)
-
-#     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#     largestContour = findLargestContour(contours)
-
-#     if largestContour is not None:
-#         x, y, w, h = cv2.boundingRect(largestContour)
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-#     # cv2.imshow('mask', mask)
-#     # mask = cv2.add(mask, np.zeros_like(mask, dtype=np.uint8))
-#     cv2.imshow('frame', frame)
-
-#     if cv2.waitKey(1) & 0xFF == ord('d'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
-# import cv2
-# import numpy as np
-
-# def findLargestContour(contours):
-#     maxArea = 0
-#     minArea = 500
-#     largestContour = None
-#     for contour in contours:
-#         area = cv2.contourArea(contour)
-#         if area > maxArea and area > minArea: 
-#             maxArea = area
-#             largestContour = contour
-#     return largestContour
-
-# lower_red1 = np.array([0,70,50])
-# upper_red1 = np.array([10,255,255])
-
-# lower_blue = np.array([110,50,50])
-# upper_blue = np.array([130,255,255])
-
-# cap = cv2.VideoCapture(0)
-
-# while True:
-#     ret, frame = cap.read()
-
-#     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-#     red_mask = cv2.inRange(hsv_frame, lower_red1, upper_red1)
-#     blue_mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
-
-#     red_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#     red_largestContour = findLargestContour(red_contours)
-#     blue_largestContour = findLargestContour(blue_contours)
-
-#     if red_largestContour is not None:
-#         red_moments = cv2.moments(red_largestContour)
-#         red_cX = int(red_moments["m10"] / red_moments["m00"])
-#         red_cY = int(red_moments["m01"] / red_moments["m00"])
-#         red_x, red_y, red_w, red_h = cv2.boundingRect(red_largestContour)
-#         cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-#         cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-
-#     if blue_largestContour is not None:
-#         blue_moments = cv2.moments(blue_largestContour)
-#         blue_cX = int(blue_moments["m10"] / blue_moments["m00"])
-#         blue_cY = int(blue_moments["m01"] / blue_moments["m00"])
-#         blue_x, blue_y, blue_w, blue_h = cv2.boundingRect(blue_largestContour)
-#         cv2.rectangle(frame, (blue_x, blue_y), (blue_x + blue_w, blue_y + blue_h), (255, 0, 0), 2)
-#         cv2.circle(frame, (blue_cX, blue_cY), 10, (255, 0, 0), -1)
-
-#     cv2.imshow('frame', frame)
-#     cv2.imshow('Red Mask', red_mask)
-    
-
-#     if cv2.waitKey(1) & 0xFF == ord('d'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
-
-# import cv2
-# import numpy as np
-
-# class Detection:
-    
-#     # def findLargestContour(contours):
-#     #     maxArea = 0
-#     #     minArea = 600
-#     #     largestContour = None
-#     #     for contour in contours:
-#     #         area = cv2.contourArea(contour)
-#     #         if area > maxArea and area > minArea: 
-#     #             perimeter = cv2.arcLength(contour, True)
-#     #             circularity = 4 * np.pi * area / (perimeter * perimeter)
-#     #             hull = cv2.convexHull(contour)
-#     #             hull_area = cv2.contourArea(hull)
-#     #             solidity = float(area) / hull_area
-#     #             if circularity > 0.7 or solidity > 0.9:
-#     #                 maxArea = area
-#     #                 largestContour = contour
-#     #     return largestContour
-
-#     def findBalls(contours):
-#         maxArea = 0
-#         minArea = 600
-#         ballsFound = []
-#         for contour in contours:
-#             area = cv2.contourArea(contour)
-#             if area > maxArea and area > minArea: 
-#                 perimeter = cv2.arcLength(contour, True)
-#                 circularity = 4 * np.pi * area / (perimeter * perimeter)
-#                 hull = cv2.convexHull(contour)
-#                 hull_area = cv2.contourArea(hull)
-#                 solidity = float(area) / hull_area
-#                 if circularity > 0.7 or solidity > 0.9:
-#                     maxArea = area
-#                     ballsFound.append[contour]
-#         return ballsFound
-
-#     # def isBallShape(contour):
-#     #     perimeter = cv2.arcLength(contour, True)
-#     #     area = cv2.contourArea(contour)
-#     #     circularity = 4 * np.pi * area / (perimeter * perimeter)
-#     #     if circularity > 0.4:  # Adjust the circularity threshold as needed, 0.7
-#     #         return True
-#     #     return False
-
-#     lower_red1 = np.array([0, 100, 100]) #0, 70, 50
-#     upper_red1 = np.array([8, 255, 255])
-
-#     # lower_red1 = np.array([0, 70, 50]) #0, 70, 50
-#     # upper_red1 = np.array([15, 255, 255])
-
-#     lower_red2 = np.array([170, 70, 50]) #170, 70, 50
-#     upper_red2 = np.array([180, 255, 255])
-
-#     # lower_red2 = np.array([160, 70, 50]) #0, 70, 50
-#     # upper_red2 = np.array([180, 255, 255])
-
-#     lower_blue = np.array([110, 100, 100])
-#     upper_blue = np.array([130, 255, 255])
-
-#     # lower_blue = np.array([100, 50, 50])
-#     # upper_blue = np.array([140, 255, 255])
-
-#     video_path = '2.mp4'  # Replace with the actual path to your video file
-#     cap = cv2.VideoCapture(video_path)
-#     # cap = cv2.VideoCapture(0)
-
-#     fps = cap.get(cv2.CAP_PROP_FPS)
-#     delay = int(1000 / fps)  # Delay corresponding to video's frame rate
-
-#     while True:
-#         ret, frame = cap.read()
-
-#         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-#         red1_mask = cv2.inRange(hsv_frame, lower_red1, upper_red1)
-#         red2_mask = cv2.inRange(hsv_frame, lower_red2, upper_red2)
-#         blue_mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
-
-#         red1_contours, _ = cv2.findContours(red1_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#         red2_contours, _ = cv2.findContours(red2_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#         blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#         # red1_largestContour = findLargestContour(red1_contours)
-#         # red2_largestContour = findLargestContour(red2_contours)
-#         # blue_largestContour = findLargestContour(blue_contours)
-
-#         red1_found = findBalls(red1_contours)
-#         red2_found = findBalls(red2_contours)
-#         blue_found = findBalls(blue_contours)
-
-#         # if red1_largestContour is not None:
-#         #     red_moments = cv2.moments(red1_largestContour)
-#         #     red_cX = int(red_moments["m10"] / red_moments["m00"])
-#         #     red_cY = int(red_moments["m01"] / red_moments["m00"])
-#         #     red_x, red_y, red_w, red_h = cv2.boundingRect(red1_largestContour)
-#         #     cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-#         #     cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-#         # elif red2_largestContour is not None:
-#         #     red_moments = cv2.moments(red2_largestContour)
-#         #     red_cX = int(red_moments["m10"] / red_moments["m00"])
-#         #     red_cY = int(red_moments["m01"] / red_moments["m00"])
-#         #     red_x, red_y, red_w, red_h = cv2.boundingRect(red2_largestContour)
-#         #     cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-#         #     cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-
-#         # if blue_largestContour is not None:
-#         #     blue_moments = cv2.moments(blue_largestContour)
-#         #     blue_cX = int(blue_moments["m10"] / blue_moments["m00"])
-#         #     blue_cY = int(blue_moments["m01"] / blue_moments["m00"])
-#         #     blue_x, blue_y, blue_w, blue_h = cv2.boundingRect(blue_largestContour)
-#         #     cv2.rectangle(frame, (blue_x, blue_y), (blue_x + blue_w, blue_y + blue_h), (255, 0, 0), 2)
-#         #     cv2.circle(frame, (blue_cX, blue_cY), 10, (255, 0, 0), -1)
-
-#         if red1_found is not None:
-#             red_moments = cv2.moments(red1_found)
-#             red_cX = int(red_moments["m10"] / red_moments["m00"])
-#             red_cY = int(red_moments["m01"] / red_moments["m00"])
-#             red_x, red_y, red_w, red_h = cv2.boundingRect(red1_found)
-#             cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-#             cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-#         elif red2_found is not None:
-#             red_moments = cv2.moments(red2_found)
-#             red_cX = int(red_moments["m10"] / red_moments["m00"])
-#             red_cY = int(red_moments["m01"] / red_moments["m00"])
-#             red_x, red_y, red_w, red_h = cv2.boundingRect(red2_found)
-#             cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-#             cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-
-#         if blue_found is not None:
-#             blue_moments = cv2.moments(blue_found)
-#             blue_cX = int(blue_moments["m10"] / blue_moments["m00"])
-#             blue_cY = int(blue_moments["m01"] / blue_moments["m00"])
-#             blue_x, blue_y, blue_w, blue_h = cv2.boundingRect(blue_found)
-#             cv2.rectangle(frame, (blue_x, blue_y), (blue_x + blue_w, blue_y + blue_h), (255, 0, 0), 2)
-#             cv2.circle(frame, (blue_cX, blue_cY), 10, (255, 0, 0), -1)
-
-#         cv2.imshow('frame', frame)
-#         cv2.imshow('Red Mask 1', red1_mask)
-#         cv2.imshow('Red Mask 2', red2_mask)
-#         cv2.imshow('Blue Mask', blue_mask)
-        
-#         # if cv2.waitKey(delay) & 0xFF == ord('d'):
-#         #     break
-#         if cv2.waitKey(1) & 0xFF == ord('d'):
-#             break
-
-#     cap.release()
-#     cv2.destroyAllWindows()
+# Color Detection With Normal Camera
+# Use Shape Filteration to Detect Ball
+# Use Noise Filteration
 
 import cv2
 import numpy as np
+import imutils
 
 class Detection:
 
@@ -301,6 +14,7 @@ class Detection:
         for contour in contours:
             area = cv2.contourArea(contour)
             if area > minArea: 
+                # Shape Fileration
                 perimeter = cv2.arcLength(contour, True)
                 approx = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
                 if len(approx) < 3 or len(approx) > 6:
@@ -316,16 +30,15 @@ class Detection:
 
     lower_red1 = np.array([0, 100, 100]) #0, 70, 50
     upper_red1 = np.array([8, 255, 255])
-
-
     lower_red2 = np.array([170, 70, 50]) #170, 70, 50
     upper_red2 = np.array([180, 255, 255])
-
 
     lower_blue = np.array([110, 50, 50])
     upper_blue = np.array([130, 255, 255])
 
-    video_path = '2.mp4'  # Replace with the actual path to your video file
+    lower_purple = np.array([130, 0, 0])
+    upper_purple = np.array([160, 255, 255])
+
     cap = cv2.VideoCapture(0)
 
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -335,31 +48,44 @@ class Detection:
         ret, frame = cap.read()
 
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        
+        gaussBlurImg = cv2.GaussianBlur(frame, (9, 9), cv2.BORDER_DEFAULT)
+        HSVResult = cv2.cvtColor(gaussBlurImg, cv2.COLOR_BGR2HSV)
 
-        red1_mask = cv2.inRange(hsv_frame, lower_red1, upper_red1)
-        red2_mask = cv2.inRange(hsv_frame, lower_red2, upper_red2)
-        blue_mask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        red1_mask = cv2.inRange(HSVResult, lower_red1, upper_red1)
+        red2_mask = cv2.inRange(HSVResult, lower_red2, upper_red2)
+        red_mask = cv2.bitwise_or(red1_mask, red2_mask)
 
-        red1_contours, _ = cv2.findContours(red1_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        red2_contours, _ = cv2.findContours(red2_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        filterErodeRed = cv2.erode(red_mask, kernel, iterations=3)
+        filterDilateRed = cv2.dilate(filterErodeRed, kernel, iterations=2)
 
-        red1_found = findBalls(red1_contours)
-        red2_found = findBalls(red2_contours)
+        blue_mask = cv2.inRange(HSVResult, lower_blue, upper_blue)
+        filterErodeBlue = cv2.erode(blue_mask, kernel, iterations=3)
+        filterDilateBlue = cv2.dilate(filterErodeBlue, kernel, iterations=2)
+
+        red_contours = cv2.findContours(filterDilateRed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        red_contours = imutils.grab_contours(red_contours)
+
+        blue_contours = cv2.findContours(filterDilateBlue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        blue_contours = imutils.grab_contours(blue_contours)
+
+        purple_mask = cv2.inRange(HSVResult, lower_purple, upper_purple)
+        filterErodePurple = cv2.erode(purple_mask, kernel, iterations=3)
+        filterDilatePurple = cv2.dilate(filterErodePurple, kernel, iterations=2)
+
+        purple_contours = cv2.findContours(filterDilatePurple, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        purple_contours = imutils.grab_contours(purple_contours)
+
+        red_found = findBalls(red_contours)
         blue_found = findBalls(blue_contours)
+        purple_found = findBalls(purple_contours)
 
-        for redBallOne in red1_found:
+        for redBallOne in red_found:
             red_moments = cv2.moments(redBallOne)
             red_cX = int(red_moments["m10"] / red_moments["m00"])
             red_cY = int(red_moments["m01"] / red_moments["m00"])
             red_x, red_y, red_w, red_h = cv2.boundingRect(redBallOne)
-            cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
-            cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
-        for redBallTwo in red2_found:
-            red_moments = cv2.moments(redBallTwo)
-            red_cX = int(red_moments["m10"] / red_moments["m00"])
-            red_cY = int(red_moments["m01"] / red_moments["m00"])
-            red_x, red_y, red_w, red_h = cv2.boundingRect(redBallTwo)
             cv2.rectangle(frame, (red_x, red_y), (red_x + red_w, red_y + red_h), (0, 0, 255), 2)
             cv2.circle(frame, (red_cX, red_cY), 10, (0, 0, 255), -1)
 
@@ -370,11 +96,20 @@ class Detection:
             blue_x, blue_y, blue_w, blue_h = cv2.boundingRect(blueBall)
             cv2.rectangle(frame, (blue_x, blue_y), (blue_x + blue_w, blue_y + blue_h), (255, 0, 0), 2)
             cv2.circle(frame, (blue_cX, blue_cY), 10, (255, 0, 0), -1)
+        
+        for purpleBall in purple_found:
+            purple_moments = cv2.moments(purpleBall)
+            purple_cX = int(purple_moments["m10"] / purple_moments["m00"])
+            purple_cY = int(purple_moments["m01"] / purple_moments["m00"])
+            purple_x, purple_y, purple_w, purple_h = cv2.boundingRect(purpleBall)
+            cv2.rectangle(frame, (purple_x, purple_y), (purple_x + purple_w, purple_y + purple_h), (255, 0, 0), 2)
+            cv2.circle(frame, (purple_cX, purple_cY), 10, (255, 0, 0), -1)
+
 
         cv2.imshow('frame', frame)
-        cv2.imshow('Red Mask 1', red1_mask)
-        cv2.imshow('Red Mask 2', red2_mask)
+        cv2.imshow('Red Mask', red_mask)
         cv2.imshow('Blue Mask', blue_mask)
+        cv2.imshow('Purple Mask', purple_mask)
         
         # if cv2.waitKey(delay) & 0xFF == ord('d'):
         #     break
